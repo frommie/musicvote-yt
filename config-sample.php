9c974370261ac152464e3d986d5b40f0af8084bf
Christian Frommert <christian.frommert@gmail.com>
2019-03-31 14:09:35 +0200

    Update config

--- config-sample.php
+++ config-sample.php
@@ -5,3 +5,4 @@ $config['db']['user']   = "dbuser";
 $config['db']['pass']   = "secret";
 $config['db']['dbname'] = "dbname";
 $config['db']['port'] = "3306";
+$config['api_key'] = "..."; // Youtube Developer API Key
